b3a27431b1257efe266a432465d9db11b209561d
Blackwell Friendly <friendlyblackwell@outlook.com>
2023-09-07 19:33:11 +0800

    init

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='configsaver',
+    name='confighammer',
     version='0.0.1',
     author='Blackwell',
     author_email='friendlyblackwell@example.com',
-    url='https://github.com/friendlyblackwell/configsaver',
+    url='https://github.com/friendlyblackwell/confighammer',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     description='Common Config Reader',
-    packages=['configsaver'],
+    packages=['confighammer'],
     install_requires=[
         "PyYAML",
     ]
